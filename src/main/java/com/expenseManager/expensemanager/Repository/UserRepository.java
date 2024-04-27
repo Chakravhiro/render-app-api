774bf04cc9654409d89af4a168da97e120d737f8
Your Name <you@example.com>
2024-04-27 23:29:25 +0530

    fix postgress alias issue

--- src/main/java/com/expenseManager/expensemanager/Repository/UserRepository.java
+++ src/main/java/com/expenseManager/expensemanager/Repository/UserRepository.java
@@ -11,38 +11,35 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
-//    @Modifying
-//    @Query(value = "UPDATE User_table e SET e.AmountPaid = e.AmountPaid + :additionalAmount WHERE e.Username = :name;", nativeQuery = true)
-//    void updateExpenseAmount(@Param("name") String expenseId, @Param("additionalAmount") float additionalAmount);
-
     @Modifying
-    @Query(value = "UPDATE User_table e SET e.Amount_Paid = e.Amount_Paid + :additionalAmount WHERE e.Username = :username", nativeQuery = true)
+    @Query(value = "UPDATE user_table SET Amount_Paid = Amount_Paid + :additionalAmount WHERE Username = :username", nativeQuery = true)
     void updateExpenseAmount(@Param("username") String username, @Param("additionalAmount") float additionalAmount);
 
     @Modifying
-    @Query(value = "UPDATE User_table e SET e.Amount_Paid = e.Amount_Paid - :additionalAmount WHERE e.Username = :username", nativeQuery = true)
+    @Query(value = "UPDATE user_table SET Amount_Paid = Amount_Paid - :additionalAmount WHERE Username = :username", nativeQuery = true)
     void updateExpenseRemoveAmount(@Param("username") String username, @Param("additionalAmount") float additionalAmount);
 
 
     @Modifying
-    @Query(value = "UPDATE User_table e SET e.Share = e.Share + :additionalAmount , e.Excess_Amount = :excessAmount, e.Due_Amount = :dueAmount WHERE e.Username = :username", nativeQuery = true)
+    @Query(value = "UPDATE user_table SET Share = Share + :additionalAmount, Excess_Amount = :excessAmount, Due_Amount = :dueAmount WHERE Username = :username", nativeQuery = true)
     void updateShareAmountwhenadd(@Param("username") String username, @Param("additionalAmount") float additionalAmount, @Param("excessAmount") float excessAmount, @Param("dueAmount") float dueAmount);
 
     @Modifying
-    @Query(value = "UPDATE User_table e SET e.Share = e.Share - :additionalAmount , e.Excess_Amount = :excessAmount, e.Due_Amount = :dueAmount WHERE e.Username = :username", nativeQuery = true)
+    @Query(value = "UPDATE User_table SET Share = Share - :additionalAmount , Excess_Amount = :excessAmount, Due_Amount = :dueAmount WHERE Username = :username", nativeQuery = true)
     void updateShareAmountwhenRemove(@Param("username") String username, @Param("additionalAmount") float additionalAmount, @Param("excessAmount") float excessAmount, @Param("dueAmount") float dueAmount);
 
     @Transactional
     @Modifying(clearAutomatically = true)
-    @Query("update User u set u.DueAmount = ?1, u.ExcessAmount = ?2 where u.Username = ?3")
+    @Query(value = "UPDATE user_table SET Due_Amount = ?1, Excess_Amount = ?2 WHERE Username = ?3", nativeQuery = true)
+
     void updateUserBalance(double DueAmount, double ExcessAmount, String Username);
 
     @Transactional
     @Modifying
-    @Query("delete from User u where u.id = ?1")
+    @Query(value = "delete from User where id = ?1",nativeQuery = true)
     void deleteByUserID(Long id);
 
-    @Query("select u from User u where u.Username = ?1")
+    @Query(value = "select from User u where Username = ?1",nativeQuery = true)
     Optional<User> findUserByName(String Username);
 
 
