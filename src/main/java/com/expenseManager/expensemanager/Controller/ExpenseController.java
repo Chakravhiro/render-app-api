7b3df205e2f41b7880deb0d7f8af1788bf345db2
Your Name <you@example.com>
2024-04-28 18:47:59 +0530

    fix cors

--- src/main/java/com/expenseManager/expensemanager/Controller/ExpenseController.java
+++ src/main/java/com/expenseManager/expensemanager/Controller/ExpenseController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin
 public class ExpenseController {
 
     @Autowired
--- src/main/java/com/expenseManager/expensemanager/Controller/UserController.java
+++ src/main/java/com/expenseManager/expensemanager/Controller/UserController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin
 public class UserController {
     @Autowired
     public UserService userService;
